cd0ad218c2aa9d71cc825481d55d25d0ac99f40a
isanthree <h1486636414@163.com>
2021-07-13 00:27:58 +0800

    事务操作（声明式事务管理「参数配置」）

--- spring5_tx1/src/com/isanthree/spring5/service/UserService.java
+++ spring5_tx1/src/com/isanthree/spring5/service/UserService.java
@@ -3,10 +3,22 @@ package com.isanthree.spring5.service;
 import com.isanthree.spring5.dao.UserDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Isolation;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
+/**
+ * Transactional(propagation = Propagation.REQUIRED) 注解：为该类里面的所有方法都添加事务
+ * Propagation：事务传播行为
+ * Isolation：事务隔离级别（REPEATABLE_READ：可重复读）
+ * timeout：超时时间（-1 是默认值，即不设置超时时间）
+ * readOnly：是否只读（默认值为 false）
+ * rollbackFor：回滚
+ * noRollbackFor：不回滚
+ */
 @Service
-@Transactional  // Transactional 注解：为该类里面的所有方法都添加事务
+@Transactional(propagation = Propagation.REQUIRED, isolation = Isolation.REPEATABLE_READ,
+        timeout = -1, readOnly = false)
 public class UserService {
     // 注入 Dao
     @Autowired
