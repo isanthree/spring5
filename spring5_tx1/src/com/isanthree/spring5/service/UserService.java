3ebdaef7a7aac4bb93b90ed627cb10b1d2cb3e8a
isanthree <h1486636414@163.com>
2021-07-12 23:42:49 +0800

    事务操作（注解声明式事务管理）

--- spring5_tx1/src/com/isanthree/spring5/service/UserService.java
+++ spring5_tx1/src/com/isanthree/spring5/service/UserService.java
@@ -3,8 +3,10 @@ package com.isanthree.spring5.service;
 import com.isanthree.spring5.dao.UserDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@Transactional  // Transactional 注解：为该类里面的所有方法都添加事务
 public class UserService {
     // 注入 Dao
     @Autowired
@@ -14,6 +16,9 @@ public class UserService {
     public void accountMoney() {
         // Lucy 少 100
         userDao.reduceMoney();
+
+        // 模拟异常，用于测试事务管理
+//        int a = 1 / 0;
 
         // Mary 多 100
         userDao.addMoney();
